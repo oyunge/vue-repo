117fa944d98ce3ccdf1f9ed36a35d99c4f8869d6
Edwin <edwinombati99@gmail.com>
2020-04-04 10:01:32 -0700

    direct commit

--- index.js
+++ index.js
@@ -1,32 +1,117 @@
 // el:is used to access Elements in the html
-var app = new Vue({
-    el: '#app',
+// var app = new Vue({
+//     el: '#app',
 
-    data:{
-        message:'hello world',
-        name:'ABC',
-        price: '2000',
-        path:'https://images.unsplash.com/photo-1553680528-62abdfec6544?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-        link: 'https://github.com/oyunge/form-repo',
-    },
-    methods:{
-        discount:function(){
-            cost = this.price -100;
-            return cost;
-        },
-        add:function(){
-            var a = 20;
-            var b = 30;
-            var c ;
+//     data:{
+//         message:'hello world',
+//         name:'ABC',
+//         price: '2000',
+//         path:'https://images.unsplash.com/photo-1553680528-62abdfec6544?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+//         link: 'https://github.com/oyunge/form-repo',
+//     },
+//     methods:{
+//         discount:function(){
+//             cost = this.price -100;
+//             return cost;
+//         },
+//         add:function(){
+//             var a = 20;
+//             var b = 30;
+//             var c ;
 
-            c = a + b;
-            return c;
-        }
+//             c = a + b;
+//             return c;
+//         }
 
-    }
-});
+//     }
+// });
+                    // VUE EVENTS
+
+                    // var app = new Vue({
+                    //  el:'#app',
+
+                    //  data:{
+                    //  temp: 0,
+
+                    //  },
+                    //  methods:{
+                    //  increase:function(){
+                    //      this.temp++;
+                    //  },
+                    //  decrease:function() {
+                    //     this.temp--; 
+                    //  }
+                    // }
+
+                    // });
 
 
+                        //   EVENT MODIFIER
+                    //      var app = new Vue({
+                    //  el:'#app',
+
+                    //  data:{
+                    //  temp: 0,
+
+                    //  },
+                    //  methods:{
+                    //  increase:function(){
+                    //      this.temp++;
+                    //  },
+                    //  decrease:function() {
+                    //     this.temp--; 
+                    //  }
+                    // }
+
+                    //  });
+
+
+                    // KEY PRESSED EVENT
+
+                    // var app = new Vue({
+                    //  el:'#app',
+                      
+                    //  data:{
+
+
+                    //  },
+                     
+                    //  keypressed:function() {
+                    //  console.log("keypressed");
+                      
+                    //  }
+                    // });
+
+                    // TWO WAY DATA BINDING
+                //    var app = new Vue({
+                //        el:'#app',
+
+                //        data:{
+
+                //         message:'',
+                //        }
+                //    });
+
+                //    DYNAMIC CSS CLASSES asssignment
+                // var app = new Vue({
+                // el:'#app',
+
+                // data:{
+                //     gate:'true',
+                   
+
+                // },
+                // gate:function(){
+                //     if(this.gate==true){
+                //        document.write("gate closed");
+                //     }
+                //     else {
+                //      document.write("gate closed");
+                //     }
+                // }
+
+
+                // });
 
     // //    VUE METHODS
     // var app = new Vue({
